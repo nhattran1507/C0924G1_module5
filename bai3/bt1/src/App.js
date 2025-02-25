b93f8ba70c3b9f27e400120e51bbcae73261bfc2
C0924G1-TRANMINHNHAT <minhnhat1507@gmail.com>
2025-02-25 08:04:29 +0700

    baitap

---
+++ bai3/bt1/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import StudentList from "./component/StudentList";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+      <div className="App">
+        <StudentList/>
+      </div>
+  );
+}
+
+export default App;
---
+++ bai3/bt2/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import DoToList from "./components/ToDoList";
+
+function App() {
+  return (
+      <div className="App">
+        <DoToList/>
+      </div>
+  );
+}
+
+export default App;
